68efb57e98cd6304c397490691f8ef48724e730c
Mohcine Boujraf <mohssine441@gmail.com>
2025-04-24 23:12:46 +0200

    changes

--- Lessons/Lesson 1/SimpleStorage.sol
+++ Lessons/Lesson 1/SimpleStorage.sol
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract SimpleStorage{
+contract SimpleStorage {
     uint256 favoriteNumer;
-
-    mapping (string => uint256) public nametoFavoriteNumber;
+    // need to change
+    mapping(string => uint256) public nametoFavoriteNumber;
 
     People[] public people;
-    
 
-    struct People{
+    struct People {
         uint256 favoriteNumer;
         string name;
     }
 
-    function store(uint256 _favoriteNumber) public  {
+    function store(uint256 _favoriteNumber) public {
         favoriteNumer = _favoriteNumber;
     }
 
     function retrieve() public view returns (uint256) {
-        return  favoriteNumer;
+        return favoriteNumer;
     }
 
-    function addPerson(uint256 _favoriteNumber,string memory _name)public {
-        people.push(People(_favoriteNumber,_name));
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        people.push(People(_favoriteNumber, _name));
         nametoFavoriteNumber[_name] = _favoriteNumber;
     }
-
 }
